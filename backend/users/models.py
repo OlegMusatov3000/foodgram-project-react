bdab0da65330746c3a9eabc65e3278db0affde74
Олег Мусатов <olegmusatov97@gmail.com>
2023-10-07 18:30:04 +0700

    Add field:"role" in default model User

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    '''Класс кастомной модели "User".'''
+
+    class UsersType(models.TextChoices):
+        '''Тип пользователей.'''
+
+        GUEST = 'guest', _('Анонимный пользователь')
+        MODERATOR = 'moderator', _('Модератор')
+        ADMIN = 'admin', _('Админ')
+
+    role = models.TextField(
+        'Пользовательская роль',
+        blank=True,
+        choices=UsersType.choices,
+        default=UsersType.GUEST
+    )
