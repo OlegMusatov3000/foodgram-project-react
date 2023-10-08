008ce6dec3625d3e3685fd438c5856d237cc2e04
Олег Мусатов <olegmusatov97@gmail.com>
2023-10-08 12:21:32 +0700

    Add model Ingredient

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -14,3 +14,12 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.SmallIntegerField()
+    units = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
